8aea08114a87b89531b745050a61ce7dbcc0280b
Krishnan J <krishnancissp@gmail.com>
2023-11-12 01:06:54 +0530

    added maven project

---
+++ src/main/java/com/bits/ticketbookingbus/TicketbookingBusApplication.java
@@ -0,0 +1,13 @@
+package com.bits.ticketbookingbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbookingBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbookingBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bits/ticketbookingbus/TicketbookingBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.ticketbookingbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookingBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
